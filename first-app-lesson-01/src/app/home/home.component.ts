9c7c24a035443b31873f179fc9306891e1e43222
samugd17 <samugd17@gmail.com>
2024-03-07 02:32:25 +0000

    project done

--- first-app-lesson-01/src/app/home/home.component.ts
+++ first-app-lesson-01/src/app/home/home.component.ts
@@ -11,36 +11,37 @@ import { HousingLocation } from '../housinglocation';
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
-    <app-housing-location *ngFor="let housingLocation of housingLocationList"
-    [housingLocation]="housingLocation"></app-housing-location>
+  <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
   </section>
   `,
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
-  readonly imgUrl = 'https://angular.io/assets/images/tutorials/faa';
-
-  // housingLocation: HousingLocation = {
-  //   id: 9999,
-  //   name: 'Test Home',
-  //   city: 'Test city',
-  //   state: 'ST',
-  //   photo: `${this.imgUrl}/example-house.jpg`,
-  //   availableUnits: 99,
-  //   wifi: true,
-  //   laundry: false,
-  // };
-
   housingLocationList: HousingLocation[] = [];
-  HousingService: HousingService = inject(HousingService)
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.HousingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
--- first-app-lesson-01/src/app/housing-location/housing-location.component.ts
+++ first-app-lesson-01/src/app/housing-location/housing-location.component.ts
@@ -1,20 +1,28 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   template: `
     <section class="listing">
-      <img [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}" class="listing-photo">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
-  styleUrl: './housing-location.component.css'
+  styleUrls: ['./housing-location.component.css'],
 })
+
 export class HousingLocationComponent {
- @Input() housingLocation!: HousingLocation;
+
+  @Input() housingLocation!: HousingLocation;
+
 }
